da90c8e00a62a673b405487bb0b6e230ea5ed81f
Arielle <ariellcollinsb@gmail.com>
2019-10-15 10:00:46 -0600

    making necessary functionality changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,33 +1,28 @@
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
-module.exports = function(app) {
+var characterList = require("../../app/data/friends")
 
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+module.exports = function (app) {
+
+
+
+  app.get("/app/data/friends", function (req, res) {
+    res.json(data);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
-
-  app.post("/api/tables", function(req, res) {
-    if (tableData.length < 5) {
+  app.post("/app/data/friends", function (req, res) {
+    if () {
       tableData.push(req.body);
       res.json(true);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  app.post("/api/clear", function() {
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
+   
 };
+
+
+// Your apiRoutes.js file should contain two routes:
+
+// A GET route with the url / api / friends.
+// This will be used to display a JSON of all possible friends.
+// A POST routes / api / friends.This will be used to handle 
+// incoming survey results.This route will also be used to handle 
+// the compatibility logic.
