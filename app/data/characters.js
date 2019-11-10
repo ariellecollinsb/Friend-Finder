34d17acc2c05c1e3d18a095df3b2683dd19adf5d
Arielle <ariellcollinsb@gmail.com>
2019-11-09 19:55:43 -0700

    changed images and adjusted css

--- app/data/characters.js
+++ app/data/characters.js
@@ -2,17 +2,17 @@ var characters = [
 
     {
         "name": "Ash Ketchum",
-        "photo": "../images/ashxyanime.png",
+        "photo": "../images/ash.jpg",
         "scores": [2, 3, 5, 3, 5, 1, 4, 1, 3, 5, 5, 3]
     },
     {
         "name": "Misty",
-        "photo": "../images/misty-pokemon.jpg",
+        "photo": "../images/misty.jpg",
         "scores": [3, 5, 2, 1, 4, 1, 2, 5, 5, 3, 2, 5]
     },
     {
         "name": "Brock",
-        "photo": "../images/brock-onix.png",
+        "photo": "../images/brock.jpg",
         "scores": [5, 2, 1, 5, 3, 5, 5, 3, 2, 3, 1, 1]
     }
 ]
